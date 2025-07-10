88e5b2b1c12afaa9c8e7f9835b87373b35a09961
maoliangcd@gmail.com <maoliangcd@gmail.com>
2025-07-10 18:41:03 +0800

    change directory

--- include/otas_serializer.h
+++ include/otas_serializer.h
@@ -1,5 +1,16 @@
-#include "otas_binary.h"
+#include <tuple>
+#include <cstdint>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <array>
 
+#include "otas_reflection.h"
 namespace otas_serializer {
 
 #define serialize(t, s) \
@@ -13,5 +24,208 @@ namespace otas_serializer {
     std::size_t offset{}; \
     deserialize_helper<remove_cvref_t<decltype(t)>>::deserialize_template(s, t, offset); \
 }(s, t) \
+
+template <class T>
+struct serialize_helper {
+    static auto serialize_template(const T &t, std::string &s, std::size_t &offset) {
+        constexpr auto count = get_member_count<T>();
+        auto members = member_tuple_helper<T, count>::tuple_view(t);
+        [&]<std::size_t... index>(std::index_sequence<index...>) {
+            ((serialize_helper<remove_cvref_t<std::tuple_element_t<index, decltype(members)>>>::serialize_template(std::get<index>(members), s, offset)), ...);
+        } (std::make_index_sequence<count>{});
+        return ;
+    }
+};
+
+template <class T>
+struct deserialize_helper {
+    static auto deserialize_template(const std::string &s, T &t, std::size_t &offset) {
+        constexpr auto count = get_member_count<T>();
+        auto members = member_tuple_helper<T, count>::tuple_view(t);
+        [&]<std::size_t... index>(std::index_sequence<index...>) {
+            ((deserialize_helper<remove_cvref_t<std::tuple_element_t<index, decltype(members)>>>::deserialize_template(s, std::get<index>(members), offset)), ...);
+        } (std::make_index_sequence<count>{});
+        return ;
+    }
+};
+
+#define GENERATE_TEMPLATE_BASIC_TYPE(type) \
+template<> \
+struct serialize_helper<type> { \
+    static auto serialize_template(const type &t, std::string &s, std::size_t &offset) { \
+        s.append(reinterpret_cast<char *>(const_cast<type *>(&t)), sizeof(t)); \
+        offset += sizeof(t); \
+        return ; \
+    } \
+}; \
+template <> \
+struct deserialize_helper<type> { \
+    static auto deserialize_template(const std::string &s, type &t, std::size_t &offset) { \
+        memcpy(&t, &s[offset], sizeof(t)); \
+        offset += sizeof(type); \
+        return ; \
+    } \
+} \
+
+GENERATE_TEMPLATE_BASIC_TYPE(int);
+GENERATE_TEMPLATE_BASIC_TYPE(unsigned int);
+GENERATE_TEMPLATE_BASIC_TYPE(long);
+GENERATE_TEMPLATE_BASIC_TYPE(unsigned long);
+GENERATE_TEMPLATE_BASIC_TYPE(long long);
+GENERATE_TEMPLATE_BASIC_TYPE(unsigned long long);
+GENERATE_TEMPLATE_BASIC_TYPE(float);
+GENERATE_TEMPLATE_BASIC_TYPE(double);
+GENERATE_TEMPLATE_BASIC_TYPE(char);
+GENERATE_TEMPLATE_BASIC_TYPE(signed char);
+GENERATE_TEMPLATE_BASIC_TYPE(unsigned char);
+
+template <>
+struct serialize_helper<std::string> {
+    static auto serialize_template(const std::string &t, std::string &s, std::size_t &offset) {
+        std::size_t size = t.length();
+        s.append(reinterpret_cast<char *>(&size), sizeof(size));
+        offset += sizeof(size);
+        s += t;
+        offset += size;
+        return ;
+    }
+};
+template <>
+struct deserialize_helper<std::string> {
+    static auto deserialize_template(const std::string &s, std::string &t, std::size_t &offset) {
+        std::size_t size;
+        memcpy(&size, &s[offset], sizeof(size));
+        offset += sizeof(size);
+        t = std::string(&s[offset], size);
+        offset += size;
+        return ;
+    }
+};
+
+#define GENERATE_TEMPLATE_ITERATOR_TYPE(type) \
+template <class T> \
+struct serialize_helper<type<T>> { \
+    static auto serialize_template(const type<T> &t, std::string &s, std::size_t &offset) { \
+        std::size_t size = t.size(); \
+        s.append(reinterpret_cast<char *>(&size), sizeof(size)); \
+        offset += sizeof(size); \
+        for (const auto &item : t) { \
+            serialize_helper<T>::serialize_template(item, s, offset); \
+        } \
+        return ; \
+    } \
+}; \
+template <class T> \
+struct deserialize_helper<type<T>> { \
+    static auto deserialize_template(const std::string &s, type<T> &t, std::size_t &offset) { \
+        std::size_t size; \
+        memcpy(&size, &s[offset], sizeof(size)); \
+        offset += sizeof(size); \
+        t.resize(size); \
+        for (auto &item : t) { \
+            deserialize_helper<T>::deserialize_template(s, item, offset); \
+        } \
+    } \
+} \
+
+GENERATE_TEMPLATE_ITERATOR_TYPE(std::vector);
+GENERATE_TEMPLATE_ITERATOR_TYPE(std::list);
+GENERATE_TEMPLATE_ITERATOR_TYPE(std::queue);
+GENERATE_TEMPLATE_ITERATOR_TYPE(std::deque);
+
+
+#define GENERATE_TEMPLATE_MAP_TYPE(type) \
+template <class T, class U> \
+struct serialize_helper<type<T, U>> { \
+    static auto serialize_template(const type<T, U> &t, std::string &s, std::size_t &offset) { \
+        std::size_t size = t.size(); \
+        s.append(reinterpret_cast<char *>(&size), sizeof(size)); \
+        offset += sizeof(size); \
+        for (const auto &pair : t) { \
+            serialize_helper<T>::serialize_template(pair.first, s, offset); \
+            serialize_helper<U>::serialize_template(pair.second, s, offset); \
+        } \
+        return ; \
+    } \
+}; \
+template <class T, class U> \
+struct deserialize_helper<type<T, U>> { \
+    static auto deserialize_template(const std::string &s, type<T, U> &t, std::size_t &offset) { \
+        std::size_t size; \
+        memcpy(&size, &s[offset], sizeof(size)); \
+        offset += sizeof(size); \
+        for (std::size_t index = 0; index < size; index++) { \
+            T fi; \
+            deserialize_helper<T>::deserialize_template(s, fi, offset); \
+            U se; \
+            deserialize_helper<U>::deserialize_template(s, se, offset); \
+            t[fi] = se; \
+        } \
+        return ;\
+    } \
+}
+
+GENERATE_TEMPLATE_MAP_TYPE(std::map);
+GENERATE_TEMPLATE_MAP_TYPE(std::unordered_map);
+GENERATE_TEMPLATE_MAP_TYPE(std::multimap);
+
+
+#define GENERATE_TEMPLATE_CONTAINER_INSERT_TYPE(type) \
+template <class T> \
+struct serialize_helper<type<T>> { \
+    static auto serialize_template(const type<T> &t, std::string &s, std::size_t &offset) { \
+        std::size_t size = t.size(); \
+        s.append(reinterpret_cast<char *>(&size), sizeof(size)); \
+        offset += sizeof(size); \
+        for (const auto &item : t) { \
+            serialize_helper<T>::serialize_template(item, s, offset); \
+        } \
+        return ; \
+    } \
+}; \
+template <class T> \
+struct deserialize_helper<type<T>> { \
+    static auto deserialize_template(const std::string &s, type<T> &t, std::size_t &offset) { \
+        std::size_t size; \
+        memcpy(&size, &s[offset], sizeof(size)); \
+        offset += sizeof(size); \
+        for (std::size_t index = 0; index < size; index++) { \
+            T item; \
+            deserialize_helper<T>::deserialize_template(s, item, offset); \
+            t.insert(item); \
+        } \
+        return ; \
+    } \
+} \
+
+GENERATE_TEMPLATE_CONTAINER_INSERT_TYPE(std::set);
+GENERATE_TEMPLATE_CONTAINER_INSERT_TYPE(std::unordered_set);
+GENERATE_TEMPLATE_CONTAINER_INSERT_TYPE(std::multiset);
+
+
+template <class T, std::size_t N>
+struct serialize_helper<std::array<T, N>> {
+    static auto serialize_template(const std::array<T, N> &t, std::string &s, std::size_t &offset) {
+        std::size_t size = t.size();
+        s.append(reinterpret_cast<char *>(&size), sizeof(size));
+        offset += sizeof(size);
+        for (const auto &item : t) {
+            serialize_helper<T>::serialize_template(item, s, offset);
+        }
+        return ;
+    }
+};
+template <class T, std::size_t N>
+struct deserialize_helper<std::array<T, N>> {
+    static auto deserialize_template(const std::string &s, std::array<T, N> &t, std::size_t &offset) {
+        std::size_t size;
+        memcpy(&size, &s[offset], sizeof(size));
+        offset += sizeof(size);
+        for (std::size_t index = 0; index < size; index++) {
+            deserialize_helper<T>::deserialize_template(s, t[index], offset);
+        }
+        return ;
+    }
+};
 
 }
