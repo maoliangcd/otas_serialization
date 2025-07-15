fe5f14ddc10999a63523967c2b79c70a7dff188d
maoliangcd@gmail.com <maoliangcd@gmail.com>
2025-07-15 23:14:34 +0800

    add effiency test

---
+++ benchmark/test_effiency_1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <chrono>
+#include "otas_serializer.h"
+
+struct Leaf {
+    int x;
+    bool operator<(const Leaf &leaf) const {
+        return x > leaf.x;
+    }
+};
+
+struct Node {
+    int a;
+    double b;
+    char c;
+    std::vector<int> d;
+    std::vector<std::vector<float>> e;
+    std::string f;
+    std::map<int, int> g;
+    std::set<Leaf> h;
+    std::optional<int> i;
+    std::pair<double, double> j;
+    std::forward_list<char> k;
+};
+
+int main() {
+    Node node0{1, 2.0, '3', {1, 2, 3, 4, 5, 6, 7, 8},
+        {{0.1, 0.2}, {0.3, 0.4, 0.5}, {0.6, 0.7, 0,8, 0.9}}, "hello world!", {{1, 2}}, 
+        {{114514}, {1919810}}, {998244353}, {1.1, 2.2},{'a', 'b'}};
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000000; i++) {
+        std::string s;
+        otas_serializer::serialize(node0, s);
+        Node node1{};
+        otas_serializer::deserialize(s, node1);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "test_effiecncy_1 time: " << duration.count() << "us" << std::endl;
+    system("pause");
+    return 0;
+}
