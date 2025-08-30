55eb962ab86c74713c785e7306764ed4adf84512
maoliangcd@gmail.com <maoliangcd@gmail.com>
2025-08-30 23:00:09 +0800

    [wip] support json serialize

--- benchmark/test_json.cpp
+++ benchmark/test_json.cpp
@@ -8,11 +8,15 @@ using namespace otas_serializer;
 
 struct Node {
     std::vector<int> a;
+    std::vector<long> b;
+    std::vector<char> c;
+    std::map<int, double> d;
 };
 
 void TEST_CASE1() {
-    Node node{{1, 2, 3, 4}};
+    Node node{{1, 2, 3, 4}, {-1, -2, -3, -4}, {'x', 'y'}, {{1, 0.1}, {2, 0.2}}};
     auto s = serialize_json(node);
+    std::cout << s << std::endl;
 }
 
 int main() {
